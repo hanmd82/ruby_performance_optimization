f527e860ae682b8c49b122fb07c0f07cd42725e0
Mingding <hanmd82@gmail.com>
2016-07-10 15:04:15 +0800

    Add wrapper module for GC metrics, and example code

---
+++ wrapper.rb
@@ -0,0 +1,30 @@
+require 'json'
+require 'benchmark'
+
+def measure(&block)
+  no_gc = ( ARGV[0] == "--no-gc" )
+
+  # collect memory allocated during library loading and code under test
+  no_gc ? GC.disable : GC.start
+
+  memory_before = `ps -o rss= -p #{Process.pid}`.to_i / 1024
+  gc_stat_before = GC.stat
+
+  time = Benchmark.realtime { yield }
+  puts ObjectSpace.count_objects
+
+  GC.start(full_mark: true, immediate_sweep: true, immediate_mark: false) unless no_gc
+  puts ObjectSpace.count_objects
+
+  gc_stat_after = GC.stat
+  memory_after = `ps -o rss= -p #{Process.pid}`.to_i / 1024
+
+  puts({
+    RUBY_VERSION => {
+      gc: no_gc ? 'disabled' : 'enabled',
+      time: time.round(2),
+      gc_count: gc_stat_after[:count] - gc_stat_before[:count],
+      memory: "%d MB" % (memory_after - memory_before)
+    }
+  }.to_json)
+end
